5ed03ee3768afcbf87eeb37176fe3528f47ec30b
Raleigh Matlock <raleighman2@gmail.com>
2016-04-08 08:34:29 -0400

    Turbo-ify

--- turbo.mk
+++ turbo.mk
@@ -6,12 +6,12 @@ TARGET_SCREEN_WIDTH := 1200
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration 
-$(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/turbo/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := broken_flo
+PRODUCT_NAME := turbo_flo
 PRODUCT_DEVICE := flo
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Nexus 7 2013
@@ -21,13 +21,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/flo/device.mk)
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
-#$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=razor \
-    BUILD_FINGERPRINT=google/razor/flo:6.0/MRA58V/2324235:user/release-keys \
-    PRIVATE_BUILD_DESC="razor-user 6.0 MRA58V 2324235 release-keys"
-
-# Broken Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Jonathan Zuniga (CrackerTeg)"
+    BUILD_FINGERPRINT=google/razor/flo:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="razor-user 6.0.1 MMB29Q 2480792 release-keys"
